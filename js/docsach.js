0dc587639e56d71bc7faa8d5734309317e1228da
ITMan <datoanh25032017@gmai.com>
2020-07-05 00:09:03 +0700

    sửa docsach

--- js/docsach.js
+++ js/docsach.js
@@ -2,9 +2,25 @@
 let listMenu = $(".list-group-item");
 
 console.log(listMenu.length)
-function view(item){
-    console.log( $(item).attr("value"));
-    let idfile= $(item).attr("value");
-    $("#ifview").attr("src","suppage/"+idfile+".html");
-    
+function view(item) {
+    console.log($(item).attr("value"));
+    let idfile = $(item).attr("value");
+    $("#ifview").attr("src", "suppage/" + idfile + ".html");
+
 }
+
+function addMenu(){
+    for (let i = 1; i <= 14; i++) {
+        $('.list-group.menu').append('<li class="list-group-item" >\
+    <a href="#" value="chuong'+ i + '" onclick="view(this);" >Chương ' + i + '</a>\
+    </li>');
+    }
+    $('.list-group.menu').append('<li class="list-group-item">\
+    <a href="#" value ="pp"  onclick="view(this);">PP</a>\
+    </li>\
+    <li class="list-group-item">\
+    <a href="#" value ="playmp3" onclick="view(this);" >Play mp3</a>\
+    </li>');
+}
+addMenu();
+
