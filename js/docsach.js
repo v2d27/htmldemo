5c8996e78ca5856f5e98d54fcac368c2b4359176
ITMan <datoanh25032017@gmai.com>
2020-07-05 20:22:16 +0700

    1.0

--- js/docsach.js
+++ js/docsach.js
@@ -3,59 +3,34 @@ let listMenu = $(".list-group-item");
 
 console.log(listMenu.length)
 function view(item) {
-   
+
     let idfile = $(item).attr("name");
-    let duoifile = $(item).attr("value") == 1 ? ".htm" :".html";
+    let duoifile = $(item).attr("value") == 1 ? ".htm" : ".html";
     $("#ifview").attr("src", "suppage/" + idfile + duoifile);
+    if ($(item).attr("value") == 0) {
+        $("#show_view").css("padding","0");
+    }else $("#show_view").css("padding","8vh");
+
+
+
 
 }
 
-function addMenu() {
-    for (let i = 1; i <= 8; i++) {
-        $('.list-group.menu').append('<li class="list-group-item" >\
-    <a href="#" value="chuong'+ i + '" onclick="view(this);" >Nội Dung ' + i + '</a>\
-    </li>');
+
+$("#btnMenu").click(function () {
+    let val = $("#pos").attr("val")
+
+    if (val == 1) {
+        $("#pos").css("position", "relative");
+        $("#pos").attr("val", "2");
+
+        $("#iconMenu").attr("src", "image/icon/study.png")
     }
-    $('.list-group.menu').append('<li class="list-group-item">\
-    <a href="#" value ="pp"  onclick="view(this);">PP</a>\
-    </li>\
-    <li class="list-group-item">\
-    <a href="#" value ="playmp3" onclick="view(this);" >Play mp3</a>\
-    </li>');
-}
-addMenu();
-//===== resize of w3school========//
-/*
-function showFrameSize() {
-    var t;
-    var width, height;
-    width = Number(w3_getStyleValue(document.getElementById("iframeResult"), "width").replace("px", "")).toFixed();
-    height = Number(w3_getStyleValue(document.getElementById("iframeResult"), "height").replace("px", "")).toFixed();
-    document.getElementById("framesize").innerHTML = "Result Size: <span>" + width + " x " + height + "</span>";
-}
-var dragging = false;
-var stack;
-function fixDragBtn() {
-
-}
-
-function dragmove(e) {
-    var percentage = (e.pageX / window.innerWidth) * 100;
-    console.log(e.pageX);
-    if (percentage > 5 && percentage < 98) {
-        var mainPercentage = 100 - percentage;
-        document.getElementById("divmenu").style.width = percentage + "%";
-        document.getElementById("ifview").style.width = mainPercentage + "%";
+    else {
+        $("#pos").css("position", "absolute");
+        $("#pos").attr("val", "1")
+        $("#iconMenu").attr("src", "image/icon/book.png")
     }
-}
 
-    
+});
 
-function dragend() {
-    if (window.addEventListener) {
-        window.addEventListener("mousemove", function (e) { dragmove(e); });
-        window.addEventListener("touchmove", function (e) { dragmove(e); });
-
-    }
-}
-*/
